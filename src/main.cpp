1ec3b4ab2f7ac6f4032556b32f495a8825e0cbf8
Jason Turner <jason@emptycrate.com>
2022-02-11 13:31:07 -0700

    Update license information

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2022 Jason Turner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -10,6 +34,8 @@
 
 static constexpr auto USAGE =
   R"(json2cpp
+
+ Copyright 2022 Jason Turner
 
     Usage:
           json2cpp <document_name> <file_name> <output_base_name>
@@ -27,7 +53,7 @@ int main(int argc, const char **argv)
     std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
       { std::next(argv), std::next(argv, argc) },
       true,// show help if requested
-      "json2cpp 0.0.1");// version string
+      "json2cpp 0.0.1 Copyright 2022 Jason Turner");// version string
 
     std::string document_name = args.at("<document_name>").asString();
     std::filesystem::path filename = args.at("<file_name>").asString();
--- src/schema_validator.cpp
+++ src/schema_validator.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2022 Jason Turner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -16,6 +40,8 @@
 
 static constexpr auto USAGE =
   R"(schema_validator
+
+ Copyright 2022 Jason Turner
 
     Usage:
           schema_validator <schema_file> <document_to_validate> [--internal]
@@ -114,7 +140,7 @@ int main(int argc, const char **argv)
     std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
       { std::next(argv), std::next(argv, argc) },
       true,// show help if requested
-      "schema_validator 0.0.1");// version string
+      "schema_validator 0.0.1 Copyright 2022 Jason Turner");// version string
 
     std::filesystem::path schema = args.at("<schema_file>").asString();
     std::filesystem::path doc = args.at("<document_to_validate>").asString();
